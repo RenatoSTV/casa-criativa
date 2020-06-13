8f8d2a3d4dd2a1926d85146b94101e163e51cefe
Renato ST <renatostv@gmail.com>
2020-06-13 18:34:50 -0300

    Final do curso
    - Criação do banco de dados utilizando sqlite3
    
    - Integração entre Front-end e Back-end
    
    -Alimentação de dados do database através do formulario implementado no site

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# casa-criativa
- Projeto criado no curso WorkshoDev || Rocketseat
+<h1 align="center">
+    <img src="https://i.imgur.com/fkffWYG.png">
+</h1>
+
+## 📧 Sobre
+
+O projeto **Casa Criativa** é site que reúne ideias para serem realizadas durante esse periodo de quarentena. Ele foi desenvolvido durante o curso "**WorkshoDev**", oferecido pela <a href="Rocketseat.com.br" target="_blanck">**Rocketseat**</a> e ministrado pelo professor <a href="https://www.gitshowcase.com/maykbrito">**Mayk Brito**</a>.
+
+## 📷 Imagens do Projeto
+
+<img src="https://i.imgur.com/Y8tEvIW.png">
+
+## ⚙ Tecnologias Utilizadas
+
+O projeto foi desenvolvido utilizando as seguintes tecnologias:
+
+- [Html](https://www.w3schools.com/html/default.asp)
+- [Css](https://www.w3schools.com/css/default.asp)
+- [Javascript](https://www.w3schools.com/js/default.asp)
+- [Nodejs](https://www.w3schools.com/nodejs/default.asp)
+- [SQL](https://www.w3schools.com/sql/default.asp)
+- [Express](http://expressjs.com/)
+- [Nodemon](https://www.npmjs.com/package/nodemon)
+- [Nunjucks](https://mozilla.github.io/nunjucks/)
+
--- server.js
+++ server.js
@@ -2,81 +2,102 @@
 const express = require("express")
 const server = express()
 
-const ideas = [
-    {
-        img:"https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        title:"Cursos de Programação",
-        category:"Estudo",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar pretium libero.",
-        url:"https://rocketseat.com.br"
-    },
-    {
-        img:"https://image.flaticon.com/icons/svg/2729/2729005.svg",
-        title:"Exercícios",
-        category:"Saúde",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar pretium libero.",
-        url:"https://rocketseat.com.br"
-    },
-    {
-        img:"https://image.flaticon.com/icons/svg/2729/2729027.svg",
-        title:"Meditação",
-        category:"Mentalidade",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar pretium libero.",
-        url:"https://rocketseat.com.br"
-    },
-    {
-        img:"https://image.flaticon.com/icons/svg/2729/2729032.svg",
-        title:"Karaokê",
-        category:"Diversão em Familia",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar pretium libero.",
-        url:"https://rocketseat.com.br"
-    },
-    {
-        img:"https://image.flaticon.com/icons/svg/2729/2729038.svg",
-        title:"Pintura",
-        category:"Criatividade",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar pretium libero.",
-        url:"https://rocketseat.com.br"
-    },
-    {
-        img:"https://image.flaticon.com/icons/svg/2729/2729048.svg",
-        title:"Recortes",
-        category:"Criatividade",
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar pretium libero.",
-        url:"https://rocketseat.com.br"
-    },
-]
+const db = require("./db")
+
+// const ideas = [
+//    
+//     {
+//         img:"https://image.flaticon.com/icons/svg/2729/2729048.svg",
+//         title:"Recortes",
+//         category:"Criatividade",
+//         description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar pretium libero.",
+//         url:"https://rocketseat.com.br"
+//     },
+// ]
 
 //configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
 
+// habilitar o uso de req.body
+server.use(express.urlencoded({extended: true}))
+
 // configuração do nunjucks
-const nunjucks= require("nunjucks")
+const nunjucks = require("nunjucks")
 nunjucks.configure("views", {
     express: server,
     noCache: true,
 })
 
-// criei uma rita "/"
+// criei uma rota "/"
 // e capturo o pedido do cliente para responder
-server.get("/", function(req, res){
+server.get("/", function (req, res) {
 
-    const reversedIdeas = [...ideas].reverse()
-    let lastIdeas = []
-    for (let idea of reversedIdeas){
-        if (lastIdeas.length < 2){
-            lastIdeas.push(idea)
+    db.all(`SELECT * FROM ideas`, function (err, rows) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no .get /!")
         }
-    }
 
-    return res.render("index.html", {ideas: lastIdeas})
+        const reversedIdeas = [...rows].reverse()
+
+        let lastIdeas = []
+        for (let idea of reversedIdeas) {
+            if (lastIdeas.length < 2) {
+                lastIdeas.push(idea)
+            }
+        }
+
+        return res.render("index.html", { ideas: lastIdeas })
+
+    })
+
+
 })
 
-server.get("/ideias", function(req, res){
+server.get("/ideias", function (req, res) {
 
-    const reversedIdeas = [...ideas].reverse()
+    db.all(`SELECT * FROM ideas`, function (err, rows){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no .get /ideas!")
+        }
 
-    return res.render("ideias.html", {ideas: reversedIdeas})
+        const reversedIdeas = [...rows].reverse()
+    
+        return res.render("ideias.html", { ideas: reversedIdeas })
+    })
+
+})
+
+server.post("/", function(req,res){
+    // Inserir dado na tabela
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES (?,?,?,?,?);
+    `
+    
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link,
+    ]
+
+    db.run(query, values, function(err){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no .post /!")
+        }
+
+        return res.redirect("/ideias")
+    })
+
 })
 
 // Liguei meu servidor na porta 3000
